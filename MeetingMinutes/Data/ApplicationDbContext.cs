b49b2fdcb1dd54c3426c02f0c6ec7efc0f6b3b3a
eleanna.kouletou <eleanna.kouletou@libramli.ai>
2022-01-09 16:24:22 +0200

    date created date updated sync

--- MeetingMinutes/Data/ApplicationDbContext.cs
+++ MeetingMinutes/Data/ApplicationDbContext.cs
@@ -4,6 +4,9 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace MeetingMinutes.Data
 {
@@ -28,5 +31,33 @@ namespace MeetingMinutes.Data
             modelBuilder.Entity<MeetingItem>().ToTable("MeetingItem");
             modelBuilder.Entity<RiskLevel>().ToTable("RiskLevel");
         }
+        public override Task<int> SaveChangesAsync(
+
+            bool acceptAllChangesOnSuccess,
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var AddedEntities = ChangeTracker.Entries()
+                .Where(E => E.State == EntityState.Added)
+                .ToList();
+
+            AddedEntities.ForEach(E =>
+            {
+                E.Property("DateCreated").CurrentValue = DateTime.Now;
+                E.Property("DateUpdated").CurrentValue = DateTime.Now;
+            });
+
+            var EditedEntities = ChangeTracker.Entries()
+                .Where(E => E.State == EntityState.Modified)
+                .ToList();
+
+            EditedEntities.ForEach(E =>
+            {
+                E.Property("DateCreated").IsModified = false;
+                E.Property("DateUpdated").CurrentValue = DateTime.Now;
+            });
+
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+
     }
 }
